c917bbb5befb8b956fb923eb133a2d2040ecf203
Dmitry Kocherga <mitya.exceed@gmail.com>
2022-05-30 10:56:19 +0300

    fixed unused styles added mixins, variables

--- src/store/initialState.tsx
+++ src/store/initialState.tsx
@@ -13,10 +13,6 @@ export const initialState: AppState = {
     {
       id: uniqueId(),
       rows: [
-        {name: 'qw', surname: 'eee', age: '23', city: 'riga', id: uniqueId()},
-        {name: 'qw', surname: 'eee', age: '23', city: 'riga', id: uniqueId()},
-        {name: 'qw', surname: 'eee', age: '23', city: 'riga', id: uniqueId()},
-        {name: 'qw', surname: 'eee', age: '23', city: 'riga', id: uniqueId()},
       ],
       isMainTable: true,
     },
